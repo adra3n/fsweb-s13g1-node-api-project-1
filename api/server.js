3873459cd999cf2648ec7ef38656ca4cc801ac7f
Sertaç Kocagil <srtc.kcgl@gmail.com>
2023-04-17 14:19:12 +0300

    first update - post&put will be added

--- api/server.js
+++ api/server.js
@@ -1,3 +1,18 @@
 // SUNUCUYU BU DOSYAYA KURUN
 
-module.exports = {}; // SERVERINIZI EXPORT EDİN {}
+const express = require('express')
+const UsersModel = require('./users/model')
+
+const server = express()
+
+server.use(express.json())
+
+server.get('/api/users', (res, req) => {
+  const users = UsersModel.getAll()
+  res.status(200).json(users)
+})
+
+// post
+// put
+
+module.exports = { server } // SERVERINIZI EXPORT EDİN {}
